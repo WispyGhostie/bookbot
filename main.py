f3261940b765b6c1b9f4d6f3ed93d1eb44e35351
WispyGhostie <wispyghostie@proton.me>
2025-03-19 22:28:51 -0400

    Final commit

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from stats import get_num_words
 from stats import get_character_use_count
+from stats import dict_sort
+import sys
 
-def get_book_text(filepath):
-    file_contents = filepath.read()
+def get_book_text(file_contents, filepath):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
     get_num_words(file_contents)
+    print("--------- Character Count -------")
     get_character_use_count(file_contents)
-
-
+    dict_sort(get_character_use_count(file_contents))
+    print("============= END ===============")
 
 def main():
-    with open("books/frankenstein.txt", "r", encoding ="utf-8") as f:
-        get_book_text(f)
-)
-
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
+    
+    with open(filepath, 'r') as f:
+        file_contents = f.read()
+        # Rename the function to better reflect what it does
+        get_book_text(file_contents, filepath)
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def get_num_words(string_name):
     split_string = string_name.split() #spliting strings by their space
     num_words = len(split_string) #counting the total strings
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
     
 
 def get_character_use_count(string_name):
@@ -13,9 +13,26 @@ def get_character_use_count(string_name):
             char[c] += 1 #incrimenting by checking
         else:
             char[c] = 1 #initializing the first count
-            
-    dict_sort(char)
+    return char
+
+
+def sort_on(dict):
+    return dict["count"]
 
 def dict_sort(book):
-    lowest_val = float("-inf")
-    if 
+    # Create an empty list to hold our dictionaries
+    da_list = []
+    # Convert each character and count to a dictionary and add to list
+    for x, count in book.items():
+        if x.isalpha():
+            # What key-value pairs should each dictionary have?
+            da_char = {"char": x, "count": count}
+            da_list.append(da_char)
+
+    da_list.sort(reverse=True, key=sort_on)
+    # Sort the list based on count values
+    # How would you sort from greatest to least?
+    
+    for x in da_list:
+        print(f"{x['char']}: {x['count']}")
+
